20ffb3fe8eab615b232e05941f46d50bf68f8312
NihitaPavlovich <decopidoge2077@1hokage.ru>
2023-05-23 22:38:38 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import math
+
+print("Введите коэффициенты для уравнения")
+print("ax^2 + bx + c = 0:")
+a = float(input("a = "))
+b = float(input("b = "))
+c = float(input("c = "))
+
+d = b ** 2 - 4 * a * c
+print('Дискриминант d =', d)
+
+if d > 0:
+    x1 = (-b + math.sqrt(d)) / (2 * a)
+    x2 = (-b - math.sqrt(d)) / (2 * a)
+    print('x1 = ', x1 )
+    print('x2 = ', x2)
+elif d == 0:
+    x = -b / (2 * a)
+    print('x = ', x)
+else:
+    print("Корней нет")
